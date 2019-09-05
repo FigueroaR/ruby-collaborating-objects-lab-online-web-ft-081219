7d25ed4ae360878f606499d3b45b5d217a993980
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-09-05 18:46:05 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -18,7 +18,7 @@ class MP3Importer
    
   def import
     array = []
-    self.files.each{|words| Song.new_by_file_name(words) } 
+    self.files.each{|words| Song.new_by_file_name(words).flat_map } 
     
     
     binding.pry 
